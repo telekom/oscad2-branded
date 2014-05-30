dd3c8c4c01817db4a152b0bae73769b9f1b7ca1c
Ronald Dauster <rpd@gido.de>
2014-05-30 23:42:43 +0200

    Javascript Web Labels and bookmarkable results page.
    Added 'JavaScript License Web Labels'
    (see http://www.gnu.org/licenses/javascript-labels.html)
    OSCAd results can now be bookmarked

--- branding/oscad_theme_telekom/t_views.py
+++ branding/oscad_theme_telekom/t_views.py
@@ -17,20 +17,56 @@
 # ------------------------------------------------------------------------------
 
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPSeeOther
+
+from oscad.views import extract_params, results_for_osuc_and_license
+from oscad.models import OSUC
 
 import oscad_data as data
 
-@view_config(route_name='internal_use_form', 
-             renderer='templates/oscad/internal_use.jinja2',
+@view_config(route_name='advice', 
+             renderer='templates/oscad/advice.jinja2',
              request_method='GET')
+def result(request):
+    license = request.matchdict['license']
+    osuc = OSUC.from_number(request.matchdict['osuc'])
+    return results_for_osuc_and_license(request, osuc, license)
+
+
+@view_config(route_name='result', request_method='POST')
+def process_form_submission(request):
+
+    license, = extract_params(request, ['license'])
+
+    if 'osuc' in request.params:
+        osuc_number, = extract_params(request, ['osuc'])
+        osuc = OSUC.from_number(osuc_number)
+
+    else:
+        recipient, type_, state, form, context = extract_params(
+            request,
+            ['recipient', 'type', 'state', 'form', 'context']
+        )
+
+        osuc = OSUC.from_attrs(recipient=recipient, type=type_, state=state,
+                               form=form, context=context)
+
+    target = request.route_path('advice', osuc=osuc.number, license=license)
+    return HTTPSeeOther(location = target)
+
+
+@view_config(route_name     = 'internal_use_form', 
+             renderer       = 'templates/oscad/internal_use.jinja2',
+             request_method = 'GET')
 def internal_use_form(request):
     return {
         'licenses': data.valid_licenses
     }
 
-@view_config(route_name='internal_use_result', 
-             renderer='templates/oscad/internal_use_result.jinja2',
-             request_method='GET')
+
+@view_config(route_name     = 'internal_use_result', 
+             renderer       = 'templates/oscad/internal_use_result.jinja2',
+             request_method = 'GET')
 def internal_use_result(request):
     internal_use = request.params['2others'] == 'false'
     unmodified = request.params['modified'] == 'false'
@@ -40,9 +76,34 @@ def internal_use_result(request):
         'can_simplify': can_simplify
     }
 
+@view_config(route_name     = 'javascript_licenses',
+             renderer       = 'templates/oscad/javascript_licenses.jinja2',
+             request_method = 'GET')
+def javascript_licenses(request):
+    jquery = {
+        'file_name'   : 'jquery-2.0.3.min.js',
+        'file_url'    : request.static_url('oscad:jquery/jquery-2.0.3.min.js'),
+        'license_name': 'MIT',
+        'license_url' : request.static_url('oscad:static/jquery/MIT-LICENSE.txt'),
+        'source_name' : 'jquery-2.0.3.js',
+        'source_url'  : request.static_url('oscad:static/jquery/jquery-2.0.3.js')
+    }
+    bootstrap = {
+        'file_name'   : 'bootstrap.min.js',
+        'file_url'    : request.static_url('oscad:bootstrap/js/bootstrap.min.js'),
+        'license_name': 'Apache-2.0',
+        'license_url' : 'http://www.apache.org/licenses/LICENSE-2.0',
+        'source_name' : 'bootstrap.js',
+        'source_url'  : request.static_url('oscad:bootstrap/js/bootstrap.js')
+    }
+    return { 
+        'javascript_libraries': [ jquery, bootstrap ]
+    }
 
 def register_views(config):
     config.add_route('internal_use_form', 'internal_use')
     config.add_route('internal_use_result', 'internal_use_result')
+    config.add_route('javascript_licenses', 'javascript_licenses')
+    config.add_route('advice', 'advice/{license}/{osuc}')
     config.scan()
 
